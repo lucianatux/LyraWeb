4ee1f2f3dc2d5cdc19986a36124bcf7316742522
lucianatux <lucianatux@yahoo.com.ar>
2023-09-28 12:07:40 -0300

    navbar

--- lyra-web/src/App.js
+++ lyra-web/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {NavBar} from "./components/NavBar";
+import { NavBar } from './components/NavBar';
 import {Footer} from "./components/Footer";
 
 
---
+++ lyra-web/src/components/NavBar.js
@@ -0,0 +1,7 @@
+
+export const NavBar = () => {
+    return (
+    <div className="navbar">Hola</div>
+    )
+  }
+
